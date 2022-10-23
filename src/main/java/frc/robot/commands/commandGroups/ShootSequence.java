7747d4fdd7b4cb2efe7cdc9ac3266a17db644044
Grif <emailgriffinnow@gmail.com>
2022-10-23 16:56:51 -0700

    Added Nested Conditional Statements to Shoot Sequence

--- src/main/java/frc/robot/commands/commandGroups/ShootSequence.java
+++ src/main/java/frc/robot/commands/commandGroups/ShootSequence.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.commandGroups;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.FeederConstants;
@@ -22,20 +24,28 @@ public class ShootSequence extends SequentialCommandGroup {
   public ShootSequence(Uptake uptake, Feeder feeder, Shooter shooter, FileLog log) {
     addCommands(
       new FileLogWrite(true, false, "ShootSequence", "shooting", log),
-      new ShooterSetVelocity(InputMode.kLastSetSpeed, shooter, log).withTimeout(1),    // Wait for shooter to be at speed
-      new FeederSetPercentOutput(FeederConstants.onPct, feeder, log),         // turn on feeder to send first ball to shooter
-      new WaitCommand(2).withInterrupt(() -> !feeder.isBallPresent()),        // turn off feeder when ball clears feeder
-      new FeederSetPercentOutput(0, feeder, log),         // turn off feeder
-      new WaitCommand(0.2),                           // Give first ball a little time to clear feeder
+      new ShooterSetVelocity(InputMode.kLastSetSpeed, shooter, log).withTimeout(1),         // Wait for shooter to be at speed
+      new FeederSetPercentOutput(FeederConstants.onPct, feeder, log),                       // turn on feeder to send first ball to shooter
+      new WaitCommand(2).withInterrupt(() -> !feeder.isBallPresent()),                      // turn off feeder when ball clears feeder
+      new FeederSetPercentOutput(0, feeder, log),                                           // turn off feeder
+      new WaitCommand(0.2),                                                                 // Give first ball a little time to clear feeder
 
-      new UptakeSetPercentOutput(UptakeConstants.onPct, false, uptake, log),  // make sure uptake is running to send second ball to feeder
-      new ShooterSetVelocity(InputMode.kLastSetSpeed, shooter, log).withTimeout(1),    // Wait for shooter to be at speed
-      new WaitCommand(0.5).withInterrupt(feeder :: isBallPresent),
-      new FeederSetPercentOutput(FeederConstants.onPct, feeder, log),         // turn on feeder to send second ball to shooter
-
-      new WaitCommand(1).withInterrupt(() -> !feeder.isBallPresent()),  // wait for second ball to shoot 
-      new WaitCommand(.15),  // Wait for ball to exit shooter
-      new FeederSetPercentOutput(0, feeder, log),           // turn off the feeder
+      new ConditionalCommand(
+        new ConditionalCommand(
+          new UptakeEjectBall(uptake, log).withTimeout(1), 
+          sequence(
+            new UptakeSetPercentOutput(UptakeConstants.onPct, false, uptake, log),          // make sure uptake is running to send second ball to feeder
+            new ShooterSetVelocity(InputMode.kLastSetSpeed, shooter, log).withTimeout(1),   // Wait for shooter to be at speed
+            new WaitCommand(0.5).withInterrupt(feeder :: isBallPresent),
+            new FeederSetPercentOutput(FeederConstants.onPct, feeder, log),                 // turn on feeder to send second ball to shooter
+            new WaitCommand(1).withInterrupt(() -> !feeder.isBallPresent()),                // wait for second ball to shoot 
+            new WaitCommand(.15)                                                            // Wait for ball to exit shooter
+          ), 
+          () -> uptake.getBallColor() == uptake.getEjectColor()),
+        new InstantCommand(), 
+        () -> uptake.isBallAtColorSensor() || uptake.isBallGoingToFeeder()),
+      
+      new FeederSetPercentOutput(0, feeder, log),                                           // turn off the feeder
 
       new FileLogWrite(true, false, "ShootSequence", "end", log)
     );
